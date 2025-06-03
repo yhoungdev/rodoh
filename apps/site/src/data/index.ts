f27ab59126212ce29c4b18dc543605d6cb2a354f
obiabo <54102389+yhoungdev@users.noreply.github.com>
2025-06-03 09:32:35 +0100

    chore(UI): update item

---
+++ apps/site/src/data/index.ts
@@ -0,0 +1,26 @@
+interface  INavLink {
+    title: string;
+    label: string;
+    path: string;
+}
+
+const NAVLINK: INavLink[] = [
+    {
+        title: "Home",
+        label: "Home",
+        path: "/"
+    },
+    {
+        title: "Features",
+        label: "Features",
+        path: "/#price"
+    },
+    {
+        title: "Pricing",
+        label: "Price",
+        path: "/"
+    }
+
+]
+
+export { NAVLINK }
